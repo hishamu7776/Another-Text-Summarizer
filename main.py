a1d0854c23fac5f7cf4e8cbe65f56e3fc303f17f
Hisham Unniyankal <uaj.hisham@gmail.com>
2022-11-22 21:55:51 +0100

    Bar plot.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import nltk
 
+import numpy as np
+
 from nltk.corpus import stopwords
 
 from preprocess import Preprocess
@@ -22,21 +24,38 @@ if __name__ == "__main__":
     preprocess.select_a_document()
     preprocess.sentence_split()
     preprocess.sentence_filter(stopwords=stopwords, occurance=2)
+    
+    raw = preprocess.sentence_vectors
+    raw_vector, raw_matrix = vectorizer.vectorize(raw)
+    density_matrix_raw = evaluator.matrix_density(raw_matrix)
+    avg_density_raw = np.sum(np.array(density_matrix_raw).flatten())/np.prod(density_matrix_raw.shape)
 
-    
-    
     stem = vectorizer.normalize(sentences=preprocess.sentence_vectors ,type="stemming")
     stem_vector, stem_matrix = vectorizer.vectorize(stem)
+    density_matrix_stem = evaluator.matrix_density(stem_matrix)
+    avg_density_stem = np.sum(np.array(density_matrix_stem).flatten())/np.prod(density_matrix_stem.shape)
 
     lemma = vectorizer.normalize(sentences=preprocess.sentence_vectors ,type="lemmatizer")
-    lemma_vector, lemma_matrix = vectorizer.vectorize(stem)
+    lemma_vector, lemma_matrix = vectorizer.vectorize(lemma)
+    density_matrix_lemma = evaluator.matrix_density(lemma_matrix)
+    avg_density_lemma = np.sum(np.array(density_matrix_lemma).flatten())/np.prod(density_matrix_lemma.shape)
 
     ultra_stem_fix_1 = vectorizer.normalize(sentences=preprocess.sentence_vectors ,type="ultra-stemming",fix=1)
     ultra_stem_fix_1_vector, ultra_stem_fix_1_matrix = vectorizer.vectorize(ultra_stem_fix_1)
-
-    raw = preprocess.sentence_vectors
-    raw_vector, raw_matrxi = vectorizer.vectorize(raw)
-
     density_matrix_fix1 = evaluator.matrix_density(ultra_stem_fix_1_matrix)
-    plots.plot_density(density_matrix_fix1)
+    avg_density_fix1 = np.sum(np.array(density_matrix_fix1).flatten())/np.prod(density_matrix_fix1.shape)
+
+    ultra_stem_fix_2 = vectorizer.normalize(sentences=preprocess.sentence_vectors ,type="ultra-stemming",fix=2)
+    ultra_stem_fix_2_vector, ultra_stem_fix_2_matrix = vectorizer.vectorize(ultra_stem_fix_2)
+    density_matrix_fix2 = evaluator.matrix_density(ultra_stem_fix_2_matrix)
+    avg_density_fix2 = np.sum(np.array(density_matrix_fix2).flatten())/np.prod(density_matrix_fix2.shape)
+    
+    '''
+    plots.plot_density(matrix = density_matrix_raw,x_tick = raw_vector, title="Raw Data")
+    plots.plot_density(matrix = density_matrix_stem,x_tick = stem_vector, title="Stemming")
+    plots.plot_density(matrix = density_matrix_lemma,x_tick = lemma_vector, title="Lemmatization")
+    plots.plot_density(matrix = density_matrix_fix1,x_tick = ultra_stem_fix_1_vector, title="Fix 1")
+    plots.plot_density(matrix = density_matrix_fix2,x_tick = ultra_stem_fix_2_vector, title="Fix 2")
+    '''
+    plots.bar_plot(y_val=[avg_density_raw,avg_density_stem,avg_density_lemma,avg_density_fix1,avg_density_fix2],x_val=["Raw","Stem","Lemma","Fix1","Fix2"],title="Average Density")
     
--- plots.py
+++ plots.py
@@ -4,10 +4,19 @@ import seaborn as sns
 
 class Plots:
     @staticmethod
-    def plot_density(m):
-        plt.imshow(m)
-        plt.show()
+    def plot_density(matrix=None,x_tick=None,title=None):
+        plt.title(title)
+        plt.ylabel("sentences")
+        plt.xlabel("words")
+        sns.heatmap(data = matrix ,xticklabels=x_tick, cbar=False) #annot=True
+        plt.savefig('./plots/'+title+'.png')
     
     @staticmethod
-    def plot():
-        return
+    def bar_plot(x_val = None, y_val = None, title = None, xlabel=None, ylabel=None, xtick=None,ytick=None):
+        plt.title(title)
+        plt.ylabel(ylabel)
+        plt.xlabel(xlabel)
+        sns.barplot(x=x_val,y=y_val)
+        plt.show()
+
+        
